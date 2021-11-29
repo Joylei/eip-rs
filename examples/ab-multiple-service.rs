ce58cbd0dacc598b93613de7247c910c8c65280a
joylei <leingliu@gmail.com>
2021-11-29 11:05:06 +0800

    add example for multiple service packet

---
+++ examples/ab-multiple-service.rs
@@ -0,0 +1,45 @@
+use std::convert::TryFrom;
+
+use anyhow::Result;
+use rseip::{
+    cip::{
+        connection::Options,
+        epath::EPath,
+        service::{CommonServices, MessageService},
+        MessageRequest,
+    },
+    client::{
+        ab_eip::{ElementCount, TagValue},
+        AbEipConnection,
+    },
+};
+
+#[tokio::main]
+pub async fn main() -> Result<()> {
+    let mut client = AbEipConnection::new_host_lookup("192.168.0.83", Options::default()).await?;
+    let mr = client
+        .multiple_service()
+        .push(MessageRequest::new(
+            0x4C,
+            EPath::from_symbol("test_car1_x"),
+            ElementCount(1),
+        ))
+        .push(MessageRequest::new(
+            0x4C,
+            EPath::from_symbol("test_car2_x"),
+            ElementCount(1),
+        ));
+    let iter = mr.send().await?;
+    for item in iter {
+        let item = item?;
+        assert_eq!(item.reply_service, 0xCC);
+        if item.status.is_err() {
+            println!("error read tag: {}", item.status);
+        } else {
+            let value = TagValue::try_from(item.data);
+            println!("tag value: {:?}", value);
+        }
+    }
+    client.close().await?;
+    Ok(())
+}
