3213711b64dddd0a9072d9c32a216f2371702bcc
joylei <leingliu@gmail.com>
2021-12-08 09:23:53 +0800

    no duplication copy

--- cip/src/codec/decode.rs
+++ cip/src/codec/decode.rs
@@ -16,7 +16,6 @@ impl<'de> Decode<'de> for IdentityObject {
     where
         D: rseip_core::codec::Decoder<'de>,
     {
-        // dynamic size, so check size
         decoder.ensure_size(33)?;
         //let product_name_len = data[32];
 
@@ -38,7 +37,7 @@ impl<'de> Decode<'de> for IdentityObject {
             product_name: {
                 let name_len = decoder.decode_u8();
                 decoder.ensure_size(name_len as usize + 1)?;
-                let data = decoder.buf_mut().copy_to_bytes(name_len as usize).to_vec();
+                let data = decoder.buf_mut().copy_to_bytes(name_len as usize);
                 String::from_utf8_lossy(&data).into_owned().into()
             },
             state: decoder.decode_u8(),
