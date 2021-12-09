eb1df5b6cdd261ea9d3c9c29b05325479394cd76
joylei <leingliu@gmail.com>
2021-12-09 10:46:24 +0800

    remove unused code

--- cip/src/service/request.rs
+++ cip/src/service/request.rs
@@ -26,11 +26,3 @@ impl<P, D> UnconnectedSend<P, D> {
         }
     }
 }
-
-#[derive(Debug)]
-pub struct SetAttributeRequestItem<T> {
-    pub id: u16,
-    /// data size in response
-    pub size: u16,
-    pub data: T,
-}
