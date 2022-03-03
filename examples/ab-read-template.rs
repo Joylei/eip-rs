2f852f5af907a8b07f6d7c05f7481fbaf0803b1b
joylei <leingliu@gmail.com>
2022-03-03 11:56:31 +0800

    fix example

--- examples/ab-read-template.rs
+++ examples/ab-read-template.rs
@@ -20,9 +20,11 @@ pub async fn main() -> Result<()> {
     //let instance_id = first_struct_instance(&mut client).await?.unwrap();
     let template = client.find_template(instance_id).await?;
     println!("template instance:\n{:?}", template);
-    let mut req = client.read_template(&template);
-    let info = req.call().await?;
-    println!("template definition:\n{:?}", info);
+    {
+        let mut req = client.read_template(&template);
+        let info = req.call().await?;
+        println!("template definition:\n{:?}", info);
+    }
     client.close().await?;
     Ok(())
 }
