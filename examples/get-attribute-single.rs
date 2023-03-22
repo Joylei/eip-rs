16e2236afb65acc9c82ebdcfbdcbba72e93b3887
joylei <leingliu@gmail.com>
2023-03-22 21:51:01 +0800

    improve get-attribute-single example

--- examples/get-attribute-single.rs
+++ examples/get-attribute-single.rs
@@ -14,18 +14,39 @@ pub async fn main() -> Result<()> {
     let mut client = EipClient::new_host_lookup("192.168.0.83")
         .await?
         .with_connection_path(PortSegment::default());
+    get_device_type(&mut client).await?;
+    get_product_name(&mut client).await?;
+    client.close().await?;
+    Ok(())
+}
+
+async fn get_device_type(client: &mut EipClient) -> anyhow::Result<()> {
+    let identity_object_class = 0x01;
+    let attr_id = 0x02; // device type
+    let path = EPath::new()
+        .with_class(identity_object_class)
+        .with_instance(1)
+        .with_attribute(attr_id);
+    // raw bytes
+    // let value: BytesHolder = client.get_attribute_single(path).await?;
+    // dbg!(value);
+    let value: u16 = client.get_attribute_single(path).await?;
+    println!("device type: {}", value);
+    Ok(())
+}
+
+async fn get_product_name(client: &mut EipClient) -> anyhow::Result<()> {
     let identity_object_class = 0x01;
     let attr_id = 0x07; // product name
-    let mut path = EPath::new()
+    let path = EPath::new()
         .with_class(identity_object_class)
-        .with_instance(1);
-    path.push(Segment::Attribute(attr_id));
+        .with_instance(1)
+        .with_attribute(attr_id);
     // raw bytes
     // let value: BytesHolder = client.get_attribute_single(path).await?;
     // dbg!(value);
     let value: ProductName = client.get_attribute_single(path).await?;
-    dbg!(value);
-    client.close().await?;
+    println!("product name: {}", value.0);
     Ok(())
 }
 
@@ -39,7 +60,6 @@ impl<'de> Decode<'de> for ProductName {
     {
         let name_len = decoder.decode_u8();
         decoder.ensure_size(name_len as usize)?;
-        dbg!(name_len);
         let data = decoder.buf_mut().copy_to_bytes(name_len as usize);
         let product_name = unsafe {
             let buf = data.as_ptr();
